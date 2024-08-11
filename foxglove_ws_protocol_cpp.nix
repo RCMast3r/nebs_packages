914b58d2803ac2e13214123cbe17ce7d283c9034
Ben Hall <rcmast3r1@gmail.com>
2024-08-10 22:24:07 -0400

    bumping foxglove ws protocol

--- foxglove_ws_protocol_cpp.nix
+++ foxglove_ws_protocol_cpp.nix
@@ -2,7 +2,7 @@
 stdenv.mkDerivation {
   pname = "foxglove-ws-protocol-cpp";
   version = "0.0.1";
-  src = src+"/cpp/foxglove-websocket";
+  src = src + "/cpp/foxglove-websocket";
   nativeBuildInputs = [ cmake ];
   propagatedBuildInputs = [ nlohmann_json websocketpp boost zlib openssl ];
 }
